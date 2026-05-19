4ddcc18afacdb106554315ce18bbf1eb035da9b9
YX14ng <mguan@fi.uba.ar>
2026-05-19 15:57:07 -0300

    feat(backend) : create POST /roles endpoint and the controllers used

--- backend/controller/roles_controller.py
+++ backend/controller/roles_controller.py
@@ -1,6 +1,6 @@
 from flask import jsonify
-from database.db import query_db
-def mostrar_roles(id_roles, nombre, nivel_administracion):
+from database.db import query_db, modify_db
+def list_roles(id_roles, name, admin_level):
     sql = "SELECT * FROM partidos"
     condition = "WHERE 1 = 1"
     params = []
@@ -9,12 +9,68 @@ def mostrar_roles(id_roles, nombre, nivel_administracion):
         condition += " AND fecha %s"
         params.append(id_roles)
 
-    if nombre:
+    if name:
         condition += " AND nombre %s"
-        params.append(nombre)
+        params.append(name)
     
-    if nivel_administracion:
+    if admin_level:
         condition += " AND nivel_administracion %s"
-        params.append(nivel_administracion)
+        params.append(admin_level)
     
     return query_db(sql + condition, params)
+
+def create_rol(data):
+    try:
+        if not data:
+            return jsonify({ "errors": [{
+                "code": "400", 
+                "message": "Bad Request", 
+                "level": "error", 
+                "description": "JSON requerido"
+            }]}), 400
+        
+        if data["nivel_administracion"] > 3 or data["nivel_administracion"] < 1:
+            return jsonify({ "errors": [{
+                "code": "400", 
+                "message": "Bad Request", 
+                "level": "error", 
+                "description": "El nivel_administracion debe ser entre 1 y 3"}]}), 400
+        
+        query_check = """
+            SELECT id_roles FROM roles 
+            WHERE 1 = 1 AND nombre=%s AND nivel_administracion=%s
+        """
+
+        existance = query_db(query_check, (
+            data["nombre"],
+            data["nivel_administracion"]
+        ))
+
+        if existance:
+            return jsonify({ "errors": [{
+                "code": "409", 
+                "message": "Conflict", 
+                "level": "error", 
+                "description": "El rol ya existe"}]}), 409
+        
+        query_insert = """
+            INSERT INTO roles (nombre, nivel_administracion)
+            VALUES (%s, %s)
+        """
+
+        rol_id = modify_db(query_insert, (
+            data["nombre"],
+            data["nivel_administracion"]
+        ))
+
+        return jsonify({
+            "mensaje": "El rol ha sido creado exitosamente",
+            "id": rol_id
+        }), 201
+    
+    except Exception as e:
+        return jsonify({ "errors": [{
+            "code": "500", 
+            "message": "Internal Server Error", 
+            "level": "error", 
+            "description": str(e)}]}), 500
--- backend/routes/route_roles.py
+++ backend/routes/route_roles.py
@@ -1,16 +1,16 @@
 from flask import Blueprint, request, jsonify
-from controller.roles_controller import mostrar_roles
+from controller.roles_controller import list_roles, create_rol
 
 roles_bp = Blueprint('roles', __name__)
 
 @roles_bp.route("/roles", methods = ["GET"])
-def list_roles():
+def list_roles_route():
     try:
         id_roles = request.args.get('id_roles', -1, type = int)
-        nombre = request.args.get('nombre')
-        nivel_administracion = request.args.get('nivel_administracion', -1, type = int)
+        name = request.args.get('nombre')
+        admin_level = request.args.get('nivel_administracion', -1, type = int)
 
-        if nivel_administracion < 1 or nivel_administracion > 3:
+        if admin_level < 1 or admin_level > 3:
             return jsonify({"error": [{
                 "code": "400",
                 "message": "Bad Request",
@@ -26,12 +26,12 @@ def list_roles():
                     "description": "El id_roles debe ser mayor a 0"
                 }]}), 400
 
-        lista_buscada = mostrar_roles(id_roles, nombre, nivel_administracion)
+        results = list_roles(id_roles, name, admin_level)
 
-        if not lista_buscada:
+        if not results:
             return ' ', 204
         
-        return jsonify({"roles encontrados"})
+        return jsonify({"roles encontrados": results})
 
     except Exception as error:
         return jsonify({"errors": [{
@@ -40,4 +40,9 @@ def list_roles():
             "level": "error", 
             "description": str(error)
         }]}), 500
+
+@roles_bp.route("/roles", methods = ["POST"])
+def create_roles_route():
+    data = request.get_json()
+    return create_rol(data)
 
