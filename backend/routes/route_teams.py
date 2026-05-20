afb930b2121a18789ca61a03ab60f23e7b62ebd4
PunchMacarrony <asantosv@fi.uba.ar>
2026-05-20 15:43:23 -0300

    feat(backend) : create teams controlles

--- backend/routes/route_teams.py
+++ backend/routes/route_teams.py
@@ -1,3 +1,29 @@
 from flask import Blueprint
 
-teams_bp = Blueprint('teams', __name__)
+from handlers.team_handler import get_team_handler, get_team_by_id_handler, get_team_by_members_handler, create_team_handler, update_team_handler, delete_team_handler
+
+teams_bp = Blueprint("teams", __name__)
+
+@teams_bp.route("/equipos", methods=["GET"])
+def get_teams():
+    return get_team_handler()
+
+@teams_bp.route("/equipos/<int:id>", methods=["GET"])
+def get_team_by_id(id):
+    return get_team_by_id_handler(id)
+
+@teams_bp.route("/equipos/integrante/<int:id_usuario>", methods=["GET"])
+def get_team_by_members(id_usuario):
+    return get_team_by_members_handler(id_usuario)
+
+@teams_bp.route("/equipos", methods=["POST"])
+def create_team():
+    return create_team_handler()
+
+@teams_bp.route("/equipos/<int:id>", methods=["PATCH"])
+def update_team(id):
+    return update_team_handler(id)
+
+@teams_bp.route("/equipos/<int:id>", methods=["DELETE"])
+def delete_team(id):
+    return delete_team_handler(id)
