46d8e516ad2bd6ff3785e99fddfc5e0e04435440
mbelenmiramontes <mmiramontes@fi.uba.ar>
2026-05-21 16:18:16 -0300

    feature(marks)-add(files): (Creo la carpeta helpers con responses, y modifico marks haciendo correciones de responsabilidades en routes y handlers(service))

--- backend/handlers/marks_handler.py
+++ backend/handlers/marks_handler.py
@@ -1,86 +1,43 @@
-from flask import jsonify, request
+from helpers.responses import error_response, success_response
 from controller.marks_controller import get_all_marks, create_mark, get_mark_by_id, patch_mark_by_id, delete_mark_by_id
 
-def marks_handler():
-
-    filters = request.args
-
+def marks_handler(filters):
     result = get_all_marks(filters)
-
     if not result["ok"]:
-        return jsonify({
-            "errors": [{
-                "code": result["code"],
-                "message": result["message"],
-                "level": "error",
-                "description": result["description"]
-            }]
-        }), result["code"]
-
-    return jsonify({
+        return error_response(result)
+    return success_response({
         "marks": result["data"]
-    }), 200
+    })
 
-
-def create_mark_handler():
-    data = request.get_json()
+def create_mark_handler(data):
     result = create_mark(data)
     if not result["ok"]:
-        return jsonify({
-            "errors": [{
-                "code": result["code"],
-                "message": result["message"],
-                "level": "error",
-                "description": result["description"]
-            }]
-        }), result["code"]
-    return jsonify({
+        return error_response(result)
+    return success_response({
         "message": result["message"]
-    }), 201
+    }, 201)
 
 def mark_handler(id_mark):
     result = get_mark_by_id(id_mark)
     if not result["ok"]:
-        return jsonify({
-            "errors": [{
-                "code": result["code"],
-                "message": result["message"],
-                "level": "error",
-                "description": result["description"]
-            }]
-        }), result["code"]
-    return jsonify({
+        return error_response(result)
+    return success_response({
         "mark": result["data"]
-    }), 200
+    })
 
 
-def patch_mark_handler(id_mark):
-    data = request.get_json()
+def patch_mark_handler(id_mark, data):
     result = patch_mark_by_id(id_mark, data)
     if not result["ok"]:
-        return jsonify({
-            "errors": [{
-                "code": result["code"],
-                "message": result["message"],
-                "level": "error",
-                "description": result["description"]
-            }]
-        }), result["code"]
-    return jsonify({
+        return error_response(result)
+    return success_response({
         "message": result["message"]
-    }), 200
+    })
 
 def delete_mark_handler(id_mark):
     result = delete_mark_by_id(id_mark)
     if not result["ok"]:
-        return jsonify({
-            "errors": [{
-                "code": result["code"],
-                "message": result["message"],
-                "level": "error",
-                "description": result["description"]
-            }]
-        }), result["code"]
-    return jsonify({
+        return error_response(result)
+    return success_response({
         "message": result["message"]
-    }), 200
+    })
