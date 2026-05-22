73508f6db3b36dea0bcea2cb83e3c0bc00b4ea19
Luciano Chirito Torres <lucianochiritotorres@gmail.com>
2026-05-22 00:57:20 -0300

    fix(backend) : corregir endpoint de advertisement

---
+++ backend/services/advertisement_service.py
@@ -0,0 +1,42 @@
+from helpers.responses import error_response, success_response
+from controllers.advertisement_controller import get_all_advertisements, create_advertisement, get_advertisement_by_id, patch_advertisement_by_id, delete_advertisement_by_id
+
+def advertisements_service(filters):
+  result = get_all_advertisements(filters)
+  if not result["ok"]:
+      return error_response(result)
+  return success_response({
+      "advertisements": result["data"]
+  })
+
+def create_advertisement_service(data):
+  result = create_advertisement(data)
+  if not result["ok"]:
+      return error_response(result)
+  return success_response({
+      "message": result["message"]
+  }, 201)
+
+def advertisement_service(id_advertisement):
+  result = get_advertisement_by_id(id_advertisement)
+  if not result["ok"]:
+      return error_response(result)
+  return success_response({
+      "advertisement": result["data"]
+  })
+
+def patch_advertisement_service(id_advertisement, data):
+  result = patch_advertisement_by_id(id_advertisement, data)
+  if not result["ok"]:
+      return error_response(result)
+  return success_response({
+      "message": result["message"]
+  })
+
+def delete_advertisement_service(id_advertisement):
+  result = delete_advertisement_by_id(id_advertisement)
+  if not result["ok"]:
+      return error_response(result)
+  return success_response({
+      "message": result["message"]
+  })
