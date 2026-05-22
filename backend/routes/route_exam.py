8c6388f4dd0a409a6441edce302bf564cc507ce9
SOFIA <sarial@fi.uba.ar>
2026-05-22 19:22:15 -0300

    feat(exams)/Arreglo exam route y exam service

--- backend/routes/route_exam.py
+++ backend/routes/route_exam.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 from services.exam_service import (
-    exams_handler,
-    exam_handler,
-    create_exam_handler,
-    patch_exam_handler,
-    delete_exam_handler
+    exams_service,
+    exam_service,
+    create_exam_service,
+    patch_exam_service,
+    delete_exam_service
 )
 
 exam_bp = Blueprint('exam', __name__)
@@ -12,25 +12,25 @@ exam_bp = Blueprint('exam', __name__)
 #GET GENERAL y GET TIPO Y FECHA------------------------------
 @exam_bp.route("/evaluaciones", methods=["GET"])
 def obtain_exams_params():
-    return exams_handler()
+    return exams_service()
 
 #GET ID------------------------------------------------------
 @exam_bp.route("/examenes/<int:id>",methods=["GET"])
 def obtain_exam_id(id_exam):
-    return exam_handler(id_exam)
+    return exam_service(id_exam)
 
 #POST--------------------------------------------------------
 @exam_bp.route("/examenes", methods=["POST"]) 
 def add_exam():
-    return create_exam_handler()
+    return create_exam_service()
 
 #PATCH ID----------------------------------------------------
 @exam_bp.route("/examenes/<int:id>", methods=["PATCH"])
 def modify_exam(id_exam):
-    return patch_exam_handler(id_exam)
+    return patch_exam_service(id_exam)
  
 
 #DELETE ID---------------------------------------------------
 @exam_bp.route("/examenes/<int:id>", methods=["DELETE"])
 def delete_exam(id_exam):
-    return delete_exam_handler(id_exam)
+    return delete_exam_service(id_exam)
