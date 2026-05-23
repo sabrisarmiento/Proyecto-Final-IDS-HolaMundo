02fa0b8cb3d1fe0a49a969befe55028221bb5cd8
Luciano Chirito Torres <lucianochiritotorres@gmail.com>
2026-05-23 16:37:20 -0300

    fix(backend) : Se borro una importacion innecesaria

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from routes import advertisements_bp, attendance_bp, exam_bp, classes_bp, exam_types_bp, marks_bp, roles_bp, students_bp, users_bp, teams_bp, materials_bp
 app = Flask(__name__)
 
