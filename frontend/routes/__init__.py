5e3c73d91fc94584c256bcafcb931a51d82d85dd
Luciano Chirito Torres <lucianochiritotorres@gmail.com>
2026-05-25 19:07:37 -0300

    feat (frontend) : calendario

--- frontend/routes/__init__.py
+++ frontend/routes/__init__.py
@@ -2,5 +2,6 @@ from .landing import landing_bp
 from .advertisements import advertisements_bp
 from .materials import materials_bp
 from .dashboard import dashboard_bp
+from .calendar import calendar_bp
 
-__all__ = ["landing_bp", "advertisements_bp", "materials_bp", dashboard_bp]
+__all__ = ["landing_bp", "advertisements_bp", "materials_bp", "dashboard_bp", "calendar_bp"]
---
+++ frontend/routes/calendar.py
@@ -0,0 +1,45 @@
+from flask import Blueprint, render_template
+import requests
+
+calendar_bp = Blueprint('calendar', __name__)
+
+@calendar_bp.route('/cronograma', methods=['GET'])
+def calendar():
+  schedule = [
+    {
+      "week": 11,
+      "classes": [
+        {
+          "type": "Teórica",
+          "date": "19/05",
+          "topics": ["Archivos de texto", "CSV"],
+          "modality": "Virtual"
+        },
+        {
+          "type": "Práctica",
+          "date": "21/05",
+          "topics": ["Archivos CSV"],
+          "modality": "Presencial"
+        }
+      ]
+    },
+    {
+      "week": 12,
+      "classes": [
+        {
+          "type": "Teórica",
+          "date": "26/05",
+          "topics": ["Bases de Datos", "SQL (Introducción)"],
+          "modality": "Virtual"
+        },
+        {
+          "type": "Práctica",
+          "date": "28/05",
+          "topics": ["Consultas SQL básicas"],
+          "modality": "Virtual"
+        }
+      ]
+    }
+  ]
+  
+  return render_template('calendar.html', schedule=schedule)
