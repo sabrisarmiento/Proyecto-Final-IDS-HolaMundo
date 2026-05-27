d97d92b0de548a3c054aa4772f813352fddc29a4
sabrusarmiento <ssarmiento@fi.uba.ar>
2026-05-27 14:43:05 -0300

    feat(backend) : add endpoints courses

--- backend/routes/route_courses.py
+++ backend/routes/route_courses.py
@@ -1,10 +1,41 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from services.courses_service import (
-  courses_service
+    courses_service,
+    course_service,
+    create_course_service,
+    patch_course_service,
+    delete_course_service
 )
+from middleware.auth_middleware import require_auth
 
 courses_bp = Blueprint('courses', __name__)
 
 @courses_bp.route('/courses', methods=['GET'])
 def get_courses():
-  return courses_service()
+    filters = {
+        "nombre": request.args.get('nombre'),
+        "cuatrimestre": request.args.get('cuatrimestre'),
+        "anio": request.args.get('anio')
+    }
+    return courses_service(filters)
+
+@courses_bp.route('/courses/<int:id_course>', methods=['GET'])
+def get_course(id_course):
+    return course_service(id_course)
+
+@courses_bp.route('/courses', methods=['POST'])
+@require_auth
+def create_course_route():
+    data = request.get_json()
+    return create_course_service(data)
+
+@courses_bp.route('/courses/<int:id_course>', methods=['PATCH'])
+@require_auth
+def patch_course_route(id_course):
+    data = request.get_json()
+    return patch_course_service(id_course, data)
+
+@courses_bp.route('/courses/<int:id_course>', methods=['DELETE'])
+@require_auth
+def delete_course_route(id_course):
+    return delete_course_service(id_course)
