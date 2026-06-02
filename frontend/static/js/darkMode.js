a87009802d591d2b13ad8cbd9c5915cfed0f60d8
SOFIA <sarial@fi.uba.ar>
2026-06-01 22:49:37 -0300

    feat/add darkmode for public ONLY

---
+++ frontend/static/js/darkMode.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleSwitch = document.querySelector('#checkbox');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme',currentTheme);
+        if (currentTheme==='dark'){
+            if (toggleSwitch) toggleSwitch.checked = true;
+        }
+
+    }
+    if (toggleSwitch) {
+        toggleSwitch.addEventListener('change', (e) => {
+            if (e.target.checked){
+                document.documentElement.setAttribute('data-theme','dark');
+                localStorage.setItem('theme', 'dark');
+            }
+            else {
+                document.documentElement.setAttribute('data-theme','light');
+                localStorage.setItem('theme', 'light');
+            }
+            
+        });
+    }
+});
