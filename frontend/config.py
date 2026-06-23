77758c461fc07e10ab05c6334748574acaf35f81
Luciano Chirito Torres <lucianochiritotorres@gmail.com>
2026-06-23 01:05:43 -0300

    delete(frontend) : getToken

--- frontend/config.py
+++ frontend/config.py
@@ -6,6 +6,3 @@ BASE_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:5000")
 def get_headers():
     token = session.get("token")
     return {"Authorization": f"Bearer {token}"} if token else {}
-
-def get_token():
-    return session.get('token')
